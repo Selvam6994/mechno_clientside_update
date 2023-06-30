c163364bd810bc359747fe7bff5037f8b3bafb4f
Selvam6994 <selvamyoor@gmail.com>
2023-06-30 21:43:35 +0530

    bug cleared

--- src/Robotprogramming.jsx
+++ src/Robotprogramming.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import topImage from "../src/assets/Images/Automation Services/Robotics/Topimage.jpg";
 import programming from "../src/assets/Images/Automation Services/Robotics/Robot Programming.jpg";
 import nachi from "../src/assets/Images/Automation Services/Robotics/Nachi.jpg";
-import yaskawa from "../src/assets/Images/Automation Services/Robotics/Yaskawa.jpg";
+// import yaskawa from "../src/assets/Images/Automation Services/Robotics/Yaskawa.jpg";
 import ScrolltoTop from "./ScrolltoTop";
 
 function Robotprogramming() {
